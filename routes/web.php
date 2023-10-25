c8f63928b37a0b7d7777ddec49d104eb72618824
Andrea Minato <andrea.minato@hotmail.it>
2023-10-25 13:01:30 +0200

    Clipping

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Models\Clip;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 
 /*
@@ -16,23 +18,33 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
+Route::get('/{slug}', function () {
+
+    $slug = request()->route()->parameter('slug');
+
+    $clip = Clip::where('slug', $slug)->select(['body'])->first();
+
+// dd(Clip::all());
+
     return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
+        'slug' => $slug,
+        'clip' => $clip,
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::post('/clip', function () {
+    $validated = request()->validate([
+        'body' => ['required', 'min:1'],
+        'slug' => ['required', 'min:1'],
+    ]);
+
+    Clip::updateOrInsert(
+        ['slug' => $validated["slug"]],
+        $validated
+    );
+
+    return back()->withInput();
 });
-
-require __DIR__.'/auth.php';
